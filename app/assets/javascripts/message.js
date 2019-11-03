e5749a2b4cc59af4ff9f237e956ba6740bbf3b61
junki789 <junkie0915@gmail.com>
2019-11-03 19:11:10 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image != null){
+      var img = `<img class="lower-message__image" src=${message.image}>`
+    }else{
+      var img = ""
+    }
+    var html = `<div class="message">
+                  <div class="upper-message">
+                  <div class="upper-message__user-name">${message.name}</div>
+                  <div class="upper-message__date">${message.created_at}</div>
+                 </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">${message.content}</p>
+                  </div>
+                  ${img}`
+                
+                 
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('#new_message')[0].reset();
+      $('.form__submit').attr('disabled', false);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  })
+})
